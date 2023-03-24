0afb562ff59e940a2d80280c525dc695b6578c47
Meetchauhan <35761989+Meetchauhan@users.noreply.github.com>
2023-03-24 11:50:38 +0530

    react new app

--- src/News.js
+++ src/News.js
@@ -6,7 +6,7 @@ import spinner from "./images/spinner.svg";
 function News(props) {
   const [newsList, setNewsList] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [pagination, setpagination] = useState(-1);
+  const [pagination, setpagination] = useState(0);
   let defaultCountry = "in";
   let defaultCategory = "top";
   let defaultLanguage = "en";
@@ -14,17 +14,25 @@ function News(props) {
   let category = !props.category ? defaultCategory : props.category;
   let language = !props.language ? defaultLanguage : props.language;
   let pages = [
-    "1679570375f95ef5a8098086758162178535898ae6",
-    "16795699659b52daf1c7e5c97316088790a8e408b4",
-    "16795685495d29beb6eab4f581316534407e4c29b8",
-    "16795674923691551b1f1f6a29f9e3cc79e94412b0",
-    "167956552984681e2e70cab0f57d96c9f0d05ab196",
+    "16796230633e386362be4555179d2561f0444aebff",
+    "1679618126827853b7408a2f3334c1e991fb3dcc66",
+    "167960802492b2a0d4686885184513f5197e4cbe6b",
+    "1679603940b42d2cbcec1d2671b007cb2f30af8161",
+    "1679602564817f47eea9da4c17b3251e7aba6c9977",
   ];
-
+  // console.log(country);
   function handleNext() {
-    setpagination(pagination + 1);
+    if (pagination >= 0) {
+      setpagination(pagination + 1);
+    }
   }
-  console.log(pages[pagination]);
+
+  function handlePre() {
+    if (pagination > 0) {
+      setpagination(pagination - 1)
+    }
+  }
+  // console.log(pages[pagination]);
 
   useEffect(() => {
     setLoading(true);
@@ -33,15 +41,15 @@ function News(props) {
       // `https://newsdata.io/api/1/news?apikey=pub_19355292bbd3ba50b2b1a59603f56332bac1a&country=in&language=en`
       // `https://newsdata.io/api/1/news?apikey=pub_1936819bd9461c6d43bb39bc52d9fc1c0b956&country=${country}&language=en&category=${category}`
       // `https://newsdata.io/api/1/news?apikey=pub_19403b0e568f259b089e0dc8b0e36018f6bcc&country=${country}&language=${language}&category=${category}`
-      // `https://newsdata.io/api/1/news?apikey=pub_19412b095a8b70b8b057353bd2cdf9b504666&country=${country}&language=${language}&category=${category}&page=${pages[pagination]}`
-      `https://newsdata.io/api/1/news?apikey=pub_19415d72f2d551d88fea08420801d0114b838&country=${country}&language=${language}&category=${category}&page=${pages[pagination]}`
+      `https://newsdata.io/api/1/news?apikey=pub_19412b095a8b70b8b057353bd2cdf9b504666&country=${country}&language=${language}&category=${category}&page=${pages[pagination]}`
+      // `https://newsdata.io/api/1/news?apikey=pub_19415d72f2d551d88fea08420801d0114b838&country=${country}&language=${language}&category=${category}&page=${pages[pagination]}`
     )
       .then((response) => response.json())
       .then((data) => {
         setLoading(false);
         setNewsList(data.results);
       });
-  }, [category, country, language, pages, pagination]);
+  }, [category, country, language,pagination]);
   // console.log(loading);
   if (loading) {
     return (
@@ -67,9 +75,9 @@ function News(props) {
             )
         )}
       </div>
-      <div>
-        <button>Previous</button>
-        <button onClick={handleNext}>Next</button>
+      <div className="pagination">
+        <button onClick={handlePre} disabled={pagination < 1}>Previous</button>
+        <button onClick={handleNext}disabled={pagination > 3}>Next</button>
       </div>
     </div>
   );
