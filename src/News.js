88486b26721bb78ca0b24e5532aea2049a540d46
Meetchauhan <35761989+Meetchauhan@users.noreply.github.com>
2023-03-25 19:22:47 +0530

    react new app

--- src/News.js
+++ src/News.js
@@ -8,12 +8,15 @@ function News(props) {
   const [loading, setLoading] = useState(true);
   const [pagination, setpagination] = useState(0);
   const [totalPage, setTotalPage] = useState();
+  const [totalResults, setTotalResults] = useState();
+
   let defaultCountry = "in";
   let defaultCategory = "top";
   let defaultLanguage = "en";
   let country = !props.country ? defaultCountry : props.country;
   let category = !props.category ? defaultCategory : props.category;
   let language = !props.language ? defaultLanguage : props.language;
+
   let pages = [
     "",
     "1679714298a2de08ea2d2a498cc1ff60d1309b07d6",
@@ -22,7 +25,7 @@ function News(props) {
     "167968519052b563a5af47082a5ed35eee56b36e58",
     "16796832181ba39f034b2806c54eb274c1fc1448ee",
   ];
-  // console.log(country);
+
   function handleNext() {
     if (pagination >= 0) {
       setpagination(pagination + 1);
@@ -34,7 +37,6 @@ function News(props) {
       setpagination(pagination - 1);
     }
   }
-  console.log(pages[pagination]);
 
   useEffect(() => {
     setLoading(true);
@@ -43,18 +45,18 @@ function News(props) {
       // `https://newsdata.io/api/1/news?apikey=pub_19355292bbd3ba50b2b1a59603f56332bac1a&country=in&language=en`
       // `https://newsdata.io/api/1/news?apikey=pub_1936819bd9461c6d43bb39bc52d9fc1c0b956&country=${country}&language=en&category=${category}`
       // `https://newsdata.io/api/1/news?apikey=pub_19403b0e568f259b089e0dc8b0e36018f6bcc&country=${country}&language=${language}&category=${category}&page=${pages[pagination]}`
-      `https://newsdata.io/api/1/news?apikey=pub_19412b095a8b70b8b057353bd2cdf9b504666&country=${country}&language=${language}&category=${category}&page=${pages[pagination]}`
-      // `https://newsdata.io/api/1/news?apikey=pub_19415d72f2d551d88fea08420801d0114b838&country=${country}&language=${language}&category=${category}&page=${pages[pagination]}`
+      // `https://newsdata.io/api/1/news?apikey=pub_19412b095a8b70b8b057353bd2cdf9b504666&country=${country}&language=${language}&category=${category}&page=${pages[pagination]}`
+      `https://newsdata.io/api/1/news?apikey=pub_19415d72f2d551d88fea08420801d0114b838&country=${country}&language=${language}&category=${category}&page=${pages[pagination]}`
     )
       .then((response) => response.json())
       .then((data) => {
         setLoading(false);
         setNewsList(data.results);
         setTotalPage(data.nextPage);
+        setTotalResults(data.totalResults)
       });
   }, [category, country, language, pagination]);
-  // console.log(loading);
-  // console.log(totalPage);
+
   if (loading) {
     return (
       <section className="loading-section">
@@ -64,8 +66,8 @@ function News(props) {
   }
   return (
     <div className="newsInner">
-      <div className="newsSec">
-        {newsList.map(
+      <div className={totalResults === 0 ? "error" : "newsSec"}>
+        {totalResults === 0 ? <div className="no-news">No News Found</div> : newsList.map(
           (news, id) =>
             // newsList.length - 1 > id && (
               <NewsList
